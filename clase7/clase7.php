b741c1047d606b342be557be6b010166471e49fe
Paula <pcastex@proyectopuravida.org>
2018-06-01 18:07:23 -0300

    formulariodos

--- clase7/clase7.php
+++ clase7/clase7.php
@@ -13,16 +13,30 @@ $autoPhp = json_decode($autoJson,true);
 echo "<br>";
 
 var_dump($autoPhp['marca']);
+echo "<br>";
+
 var_dump($autoPhp['color']);
+
+echo "<hr>";
+
 
 
  $miArray = [ 'a'=>1, 'b'=>2, 'c'=>'Yo <3 JSON'];
   var_dump ($miArray);
+  echo "<br>";
 
   $miArrayJason = json_encode($miArray);
   echo $miArrayJason;
+  echo "<br>";
+  $deJSONaArray = json_decode($miArrayJason,true);
+  echo $deJSONaArray;
+  echo "<br>";
+  echo "<hr>";
 
-  $deJSONaArray = ;
 
+  foreach ($deJSONaArray as $value) {
+    echo  $value;
+
+  }
 
  ?>
