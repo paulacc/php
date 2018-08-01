a8c906664afd5517f70b3c136bcc946cd22ae9b0
paulacc <paulacasx@hotmail.com>
2018-07-31 22:00:21 -0300

    pp

--- TPOBJETOS/Pelicula.php
+++ TPOBJETOS/Pelicula.php
@@ -1,56 +1,59 @@
 <?php
       class Pelicula{
-         private  $id;
-         private  $title;
-         //private  $genero;
-         private  $awards;
-         private  $rating;
+         private $id;
+         private $title;
+         private $genreId;
+         private $awards;
+         private $rating;
 
 
-        public function __construct($id,$title,$awards,$rating){
-                $this->id = $id;
-                $this->title = $title;
-                $this->genero = $genero;
-                $this->awards = $awards;
-                $this->rating = $rating;
+        public function __construct($id, $title, $awards, $rating){
+          $this->id = $id;
+          $this->title = $title;
+          $this->awards = $awards;
+          $this->rating = $rating;
+        }
 
-                }
+        // Getters
+        public function getId() {
+          return $this->id;
+        }
 
-                  public function getId() {
-                  return $this->id;
-                 }
+         public function getTitle(){
+           return $this->title;
+         }
 
-                 public function geTitle(){
-                   return $this->title;
-                 }
+         public function getGenero(){
+          return $this->genero;
+         }
 
-                 //public function getGenero(){
-                  // return $this->genero;
-                 //}
+         public function getAwards(){
+           return $this->awards;
+         }
 
-                 public function getAwards(){
-                   return $this->awards;
-                 }
+         public function getRating(){
+           return $this->rating;
+         }
 
-                 public function getRating(){
-                   return $this->rating;
-                 }
+         // Setters
+         public function setGenreId($genreId) {
+           $this->genreId = $genreId;
+         }
+
+         // Other methods
+         public function Guardar(){
+
+          try {
+            $sql = "INSERT INTO `movies` 	(`id`,`title`, `rating`, `awards`)";
+            $query = $db->prepare($sql);
+            $query->execute();
+            //$datosmovies = $query->fetchAll(PDO::FETCH_ASSOC);
+          }
+          catch( PDOException $Exception ) {
+          }
 
 
-
-                 public function Guardar(){
-
-                  try {
-                    $sql = "INSERT INTO `movies` 	(`id`,`title`, `rating`, `awards`)";
-                    $query = $db->prepare($sql);
-                    $query->execute();
-                    //$datosmovies = $query->fetchAll(PDO::FETCH_ASSOC);
-                  }
-                  catch( PDOException $Exception ) {
-                  }
-
-
-                }
+        }
 
 
 
--- TPOBJETOS/peliculas.php
+++ TPOBJETOS/peliculas.php
@@ -1,34 +1,76 @@
 <?php
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
 
 require_once 'Pelicula.php';
 
-class Peliculas {
-  public static function TraerPeliculas() {
+class Peliculas
+{
+
+  public static function TraerPeliculas()
+  {
 
     require 'conexion.php';
 
-    $Busqueda= "SELECT * FROM movies";
-    $ConsultaSql= $db->prepare($Busqueda);
+    $Busqueda = "SELECT * FROM movies";
+    $ConsultaSql = $db->prepare($Busqueda);
     $ConsultaSql->execute();
     $Resultado = $ConsultaSql->fetchAll(PDO::FETCH_ASSOC);
-    $TodasLasPeliculas =[];
+    $TodasLasPeliculas = [];
 
     foreach ($Resultado as $value) {
-      $pelicula = new Pelicula($value["title"],$value["awards"])
-      $TodasLasPeliculas[] = $pelicula;
+      $objPelicula = new Pelicula($value["id"], $value["title"], $value["awards"], $value["rating"]);
+      $objPelicula->setGenreId($value['genre_id']);
+
+      $TodasLasPeliculas[] = $objPelicula;
     }
 
     return  $TodasLasPeliculas;
   }
+
+  public static function TraerPeliculasPorId($id)
+  {
+    // me conecto a la base y busco según el id que me llega
+    require 'conexion.php';
+
+    $busqueda = "SELECT * FROM movies WHERE id = {$id}";
+    $consultaSql = $db->prepare($busqueda);
+    $consultaSql->execute();
+    $resultado = $consultaSql->fetch(PDO::FETCH_ASSOC);
+
+    // Verifico si encontramos registros
+    if ($resultado) {
+      // Si encontramos armo un array de objetos pelicula
+      $objPelicula = new Pelicula($resultado["id"], $resultado["title"], $resultado["awards"], $resultado["rating"]);
+      $objPelicula->setGenreId($resultado['genre_id']);
+    } else {
+      // Si no encontramos devuelvo false
+      $objPelicula = false;
+    }
+
+    return  $objPelicula;
+  }
+
+  public static function TraerPeliculasPorTitulo($titulo) {
+    // me conecto a la base y busco por título
+      // ver de usar like "%%"
+      // SELECT * FROM movies_db.movies where title like "%la%";
+
+    // si me llegan resultados devuelvo un array de objetos Pelicula
+    // en caso contrario devuelvo false
+  }
 }
 
 
-  $pelis = Peliculas::TraerPeliculas();
-  var_dump($pelis);
-  exit;
+  $prueba = Peliculas::TraerPeliculas();
+  if ($prueba) {
+    foreach ($prueba as $pelicula) {
+?>
+  <article>
+    <h4><?php echo $pelicula->getTitle(); ?></h4>
+    <p>Rating: <?php echo $pelicula->getRating(); ?></p>
+    <hr>
+  </article>
+<?php
+    }
+  }
 
-
-
- ?>
+?>
