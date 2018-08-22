9fcdfb441c3ce6462e2688f0e7f0f93a66531f9c
paulacc <paulacasx@hotmail.com>
2018-08-21 21:58:13 -0300

    laravel

---
+++ test/app/Tarea.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Tarea extends Model
+// con el nombre de la clase Tarea va en busca de la tabla tareas en minuscula  y plural  si no se usa las convenciones
+// se puede poner un atributo  como protected $table = "remises";
+// y por cada campo crea un atributo 
+
+{
+    //
+}
---
+++ test/resources/views/tareas/lista.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Lista de tareas</title>
+	<link rel="stylesheet" type="text/css" href="css/estilo.css">
+</head>
+<body>
+
+	<h1>Listado de las tareas</h1>
+
+	<div class="tareas">
+		<div class="tarea">
+			Tarea 1
+		</div>
+		<div class="tarea">
+			Tarea 2
+		</div>
+		<div class="tarea">
+			Tarea 3
+		</div>
+	</div>
+
+</body>
+</html>
